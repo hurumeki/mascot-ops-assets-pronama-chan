8eba164057e83c529ba050db8ad0152c5467af8f
hurumeki <hurumeki1987@gmail.com>
2016-01-03 13:03:23 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mascot Ops Assets Pronama Chan
+mascot-ops用のプロ生ちゃんデータセットです。
+
+## License
+以下のものを使わせていただいています。
+
+* プロ生ちゃん (image, icon)  
+  [ガイドライン](http://pronama.azurewebsites.net/pronama/guideline/)
---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  icon: ['node_modules', 'mascot-ops-assets-pronama-chan', 'icon', 'icon.png'].join("/"),
+  image: ['node_modules', 'mascot-ops-assets-pronama-chan', 'images', 'sd06.png'].join("/")
+}
